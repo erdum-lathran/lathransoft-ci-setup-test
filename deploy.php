f7726fd22b8d5323778a69808ac3c04657703f70
erdum-lathran <erdum.adnan@lathran.com>
2025-07-22 10:02:22 +0500

    Create deploy.php

---
+++ deploy.php
@@ -0,0 +1,18 @@
+<?php
+
+$tar_file = __DIR__.'/project.tar.gz';
+$extract_to = __DIR__.'/../';
+
+run_command("tar -xf $tar_file -C $extract_to");
+// run_command('rm -f ./.ftp-deploy-sync-state.json');
+run_command('rm -f ./deploy.php');
+exit('Deployment script successfully executed');
+
+function run_command($command)
+{
+    exec($command, $output, $return_code);
+
+    if ($return_code != 0) {
+        exit('<pre>'.print_r([$command, $output, $return_code], true).'</pre>');
+    }
+}
