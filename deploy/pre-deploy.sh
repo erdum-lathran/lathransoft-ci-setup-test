ef46cdd9ed2af6ef6e238c16d42f3737a221774b
erdum-lathran <erdum.adnan@lathran.com>
2025-07-22 13:54:01 +0500

    Fix: Only uploading simple distribution
    Without any backup mechanism

--- deploy/pre-deploy.sh
+++ deploy/pre-deploy.sh
@@ -9,11 +9,3 @@ git checkout $DEPLOY_BRANCH
 npm ci
 
 npm run build
-
-tar -czf /tmp/dist.tar.gz -C ./dist .
-
-mkdir ./transfer
-
-mv /tmp/dist.tar.gz ./transfer/
-
-mv $DEPLOY_DIRECTORY ./transfer/
