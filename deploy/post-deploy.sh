a1e07800f6ad6ccf2b50258349a1fae1fbdef843
erdum-lathran <erdum.adnan@lathran.com>
2025-07-23 15:36:11 +0500

    Update post-deploy.sh

--- deploy/post-deploy.sh
+++ deploy/post-deploy.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 cd "$(dirname "$0")"
 
-tar -xzf ../dist.tar.gz -C ../
+tar -xzf ../dist.tar.gz -C ../../
 
-tar -xzf ../dist-node-modules.tar.gz -C ../
+tar -xzf ../dist-node-modules.tar.gz -C ../../
